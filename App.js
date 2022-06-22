e61b55688a5bfb9b7aa69bbf07031a8f7e7ec8bc
Wiphawanyaun <gitwiphawan@gmail.com>
2022-06-22 11:17:53 +0700

    new commit

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import Logo from "./components/Logo";
-/*import styles from "./components/styles";*/
-import stySheet from "./components/stySheet";
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import Logo from './components/Logo';
+import Stysheet from './components/Stysheet';
 
 const App = () => {
   return (
-    <View style ={stySheet.container}>
-      <Text style={[stySheet.title]}>React Native</Text>
-   
+    <View style={styles.container}>
+      {/* <Text style={[styles.title,styles.warning]}>Hello React Native</Text> */}
+      {/* <Logo /> */}
+      <Stysheet/>
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+  },
+  warning: {
+    color: 'red'
+  }
+});
 
--- components/stySheet.js
+++ components/stySheet.js
@@ -1,23 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Stysheet = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native</Text>
+    </View>
+  )
+}
+
+export default Stysheet
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1, 
-        padding:24,
-        backgroundColor: "#eaeaea"
+    container: {
+      flex: 1,
+      padding: 24,
+      backgroundColor: "#eaeaea"
     },
-    title:{
-        marginTop:16,
-        paddingVertical: 8,
-        borderWidth:4,
-        borderColor: "#20232a",
-        borderRadius: 6,
-        backgroundColor: "#61dafb",
-        color: "#20232a",
-        textAlign: "center",
-        fontSize:30,
-        fontWeight: "bold"
+    title: {
+      marginTop: 16,
+      paddingVertical: 8,
+      borderWidth: 4,
+      borderColor: "#20232a",
+      borderRadius: 6,
+      backgroundColor: "#61dafb",
+      color: "#20232a",
+      textAlign: "center",
+      fontSize: 30,
+      fontWeight: "bold"
     }
-});
-
-export default styles;
+  });
